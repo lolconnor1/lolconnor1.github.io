7f986f85b428d9940af6f91c74a0bcbb5a8e237b
connor <connorchan88@gmail.com>
2025-04-23 23:35:42 -0400

    added GenGO page

--- javaScript/projects.js
+++ javaScript/projects.js
@@ -11,26 +11,3 @@ const observer = new IntersectionObserver(entries => {
 
 faders.forEach(el => observer.observe(el));
 
-
-
-//header shit
-let lastScrollTop = 0;
-const header = document.getElementById("main-header");
-
-window.addEventListener("scroll", () => {
-  let scrollTop = window.scrollY || document.documentElement.scrollTop;
-
-  if (scrollTop > lastScrollTop && scrollTop > 80) {
-    // Scrolling down
-    header.classList.add("hidden");
-  } else {
-    // Scrolling up
-    header.classList.remove("hidden");
-  }
-
-  lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // For mobile scroll bounce
-});
-
-document.getElementById('hover-zone').addEventListener('mouseenter', () => {
-  header.classList.remove('hidden');
-});
